3112d7b5eddd2f557aa3f12ee3eb746d5e065089
Jorge Calavia <1184336+jcalavia@users.noreply.github.com>
2022-03-04 17:09:25 +0100

    New: Moved validations to pre-commit instead workflow action (#3)
    * New: Moved validations to pre-commit instead workflow action
    
    * New: Add workflows to  perform TFLint and TFSec. Configured pre-commit hooks to local verification of workflow checkes
    
    * New: configured tflint plugin AWS
    
    * Fix: updated vtflint version in github action
    
    * Fix: Reverted to oficial TFLint example

--- .devcontainer/post-instal.sh
+++ .devcontainer/post-instal.sh
@@ -10,3 +10,12 @@ curl ${LATEST_URL} > /tmp/terraform.zip
 
 cd /usr/local/bin && sudo unzip /tmp/terraform.zip
 echo "Installed: `/usr/local/bin/terraform version`"
+
+pip install --upgrade pip
+pip install pre-commit checkov
+
+
+curl -s https://raw.githubusercontent.com/terraform-linters/tflint/master/install_linux.sh | bash
+go install github.com/aquasecurity/tfsec/cmd/tfsec@latest
+
+pre-commit install
